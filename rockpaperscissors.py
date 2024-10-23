02defcb36fc45261cf2935a7a6f46e9b84dc85da
alidgrmc <alidogramaci16@gmail.com>
2024-10-23 17:50:15 +0300

    Update rockpaperscissors.py

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -1,24 +1,24 @@
 from random import randint
 from time import sleep 
                                      #TITLE
-print("_"*20,"TAŞ KAĞIT MAKAS V2.0","_"*20,"Developed by Ali DOĞRAMACI")
-print("ÇIKMAK İÇİN CTRL+C")
-print("TAŞ İÇİN 1\nKAĞIT İÇİN 2\nMAKAS İÇİN 3")
+print("_"*20,"ROCK PAPER SCISSORS","_"*20,"Developed by alidgrmc")
+print("CTRL+C FOR EXIT")
+print("Enter '1' for Rock\nEnter '2' for Paper\nEnter '3' for Scissors")
 
 win=lose=draw=0
-dictWord={1:"TAŞ",2:"KAĞIT",3:"MAKAS"} 
+dictWord={1:"Rock",2:"Paper",3:"Scissors"} 
 def take():         #Taking userdecision
     while True:
         try:
-            q=int(input("Seçiniz =>"))
-            if 1<=q<=3:
-                return q 
+            choice=int(input("Your Choice =>"))
+            if 1<=choice<=3:
+                return choice
                 break
             else:
-                print("Geçerli Bir değer giriniz!")
+                print("Enter a valid value!")
                 sleep(0.75)
         except ValueError:
-            print("Geçerli Bir değer giriniz!")
+            print("Enter a valid value!")
             sleep(0.75)
 try:      
     while True:        #main function
@@ -26,27 +26,27 @@ try:
         pcDec=randint(1,3)
         print("(.....)")
         sleep(0.5)
-        print("Sizin seçiminiz:{0}".format(dictWord.get(userDec)))
+        print("Your choice:{0}".format(dictWord.get(userDec)))
         sleep(0.25)
-        print("Bilgisayarın seçimi:{0}".format(dictWord.get(pcDec)))
+        print("Computer's choice:{0}".format(dictWord.get(pcDec)))
         if userDec==pcDec:
             sleep(0.25)
-            print("BERABERE :|")
+            print("Draw :|")
             draw+=1
-            print(f"Galibiyet={win} Mağlubiyet={lose} Beraberlik={draw}")
+            print(f"Win={win} Lose={lose} Tie={draw}")
             sleep(0.75)
         elif (userDec==1 and pcDec==3) or (userDec==2 and pcDec==1) or (userDec==3 and pcDec==2):        
             sleep(0.25)
-            print("KAZANDIN :)")
+            print("You Win :)")
             win+=1
-            print(f"Galibiyet={win} Mağlubiyet={lose} Beraberlik={draw}")
+            print(f"Win={win} Lose={lose} Tie={draw}")
             sleep(0.75)
         else:
             sleep(0.75)
-            print("KAYBETTİN :(")
+            print("You lose :(")
             lose+=1
-            print(f"Galibiyet={win} Mağlubiyet={lose} Beraberlik={draw}")
+            print(f"Win={win} Lose={lose} Tie={draw}")
             sleep(0.75)
 except KeyboardInterrupt:
-    print("\nOyundan çıkılıyor...") 
+    print("\nThanks for playing...") 
     sleep(1)
